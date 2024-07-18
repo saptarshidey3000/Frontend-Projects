03edf0aefa42f84ae1397117333d5b8dd90da707
Saptarshi Dey <117742423+saptarshidey3000@users.noreply.github.com>
2024-07-18 20:04:48 +0530

    Add files via upload

---
+++ headset-project/app.js
@@ -0,0 +1,20 @@
+function changeColor(color){
+    document.body.style.backgroundColor= color
+    let src= './images/'
+
+    const img= document.getElementById("img")
+    img.src= src + color + '.png'
+
+    document.body.classList = ""
+    if(color == 'blue'){
+        document.body.classList.add("blue")
+        document.body.style.background= '#7C93C3'
+    }
+    else if(color == 'black'){
+        document.body.classList.add("black")
+    }else{
+        document.body.classList.add("white")
+        document.body.style.background= '#eee'
+    }
+
+}
